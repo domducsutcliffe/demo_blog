04368156063de68fc86be8e3ef706e535be8796f
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-01-22 16:19:41 +0000

    Index, show and edit work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,57 @@
 class PostsController < ApplicationController
+    # before_action :find_post, only: [:show, :update, :edit, :destroy]
 
-    def index
-    end
+   def index
+       @posts = Post.all.order("created_at DESC")
+   end
 
-    def new
-        @post = Post.new
-    end
+   def new
+       @post = Post.new
+   end
 
-    def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to @post
-        else
-            render 'new'
-        end
-    end
+   def create
+       @post = Post.new(post_params)
 
-    private
+       if @post.save
+           redirect_to @post
+       else
+           render 'new'
+       end
+   end
 
-    def post_params
-        params.require(:post).permit(:title, :content)
-    end
+   def show
+    find_post
+   end
 
+   def update
 
+       if @post.update(post_params)
+           redirect_to @post
+       else
+           render 'edit'
+       end
+   end
 
-    def show
-        @post = Post.find(params[:id])
-    end
+   def edit
+    find_post
+   end
+
+   def destroy
+       find_post
+       @post.destroy
+
+       redirect_to posts_path
+
+   end
+
+   private
+
+   def post_params
+       params.require(:post).permit(:title, :content)
+   end
+
+   def find_post
+   	@post = Post.find(params[:id])
+   end
 
 end
