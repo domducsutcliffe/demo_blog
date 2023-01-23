5a6a68b1a4b322fe882734de8ee83074acc660a6
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-01-23 15:57:57 +0000

    Added full comments functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_160623) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_152803) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
